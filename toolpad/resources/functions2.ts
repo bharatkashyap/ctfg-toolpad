7ce7bbcb0c62d706d7c72b74f30b1eb945a4d673
Bharat Kashyap <bharatkashyap@outlook.com>
2023-12-27 15:19:16 +0530

    debug: revert to secrets

--- toolpad/resources/functions2.ts
+++ toolpad/resources/functions2.ts
@@ -2,6 +2,8 @@ import { Upload } from "@aws-sdk/lib-storage";
 import { S3 } from "@aws-sdk/client-s3";
 import Airtable from "airtable";
 import { promises as fs } from "fs";
+import { promisify } from "util";
+import { exec } from "child_process";
 
 // Function to download an image from a URL using native fetch
 const downloadImage = async (url, localPath) => {
@@ -16,25 +18,29 @@ const downloadImage = async (url, localPath) => {
 };
 
 // Function to upload a file to S3
-const uploadToS3 = async (airtableListingId, airtableUploadedImages) => {
+const uploadToS3 = async (
+  airtableListingId,
+  airtableUploadedImages,
+  secretsFilePath,
+  passphraseFilePath
+) => {
   try {
     // Read secrets from the encrypted file
-    if (
-      !process.env.AWS_ACCESS_KEY_ID ||
-      !process.env.AWS_SECRET_ACCESS_KEY ||
-      !process.env.AWS_REGION ||
-      !process.env.AWS_S3_BUCKET
-    ) {
-      throw new Error("AWS Credebntials not available.");
-    }
+    const {
+      AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY,
+      AWS_REGION,
+      AWS_S3_BUCKET,
+    } = await readSecretsFromFile(secretsFilePath, passphraseFilePath);
 
     // Configure AWS SDK with the acquired information
     const s3 = new S3({
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
       },
-      region: process.env.AWS_REGION,
+
+      region: AWS_REGION,
     });
 
     const uploadPromises = airtableUploadedImages.map(async (image) => {
@@ -45,7 +51,7 @@ const uploadToS3 = async (airtableListingId, airtableUploadedImages) => {
       const fileContent = await fs.readFile(image.id);
       const s3Key = `screenshots/${airtableListingId}/${image.id}`;
       const params = {
-        Bucket: process.env.AWS_S3_BUCKET,
+        Bucket: AWS_S3_BUCKET,
         Key: s3Key,
         Body: fileContent,
       };
@@ -57,12 +63,12 @@ const uploadToS3 = async (airtableListingId, airtableUploadedImages) => {
 
       console.info(
         "Image uploaded successfully:",
-        process.env.AWS_S3_BUCKET,
-        process.env.AWS_REGION,
+        AWS_S3_BUCKET,
+        AWS_REGION,
         response.Key
       );
 
-      return `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET}/${s3Key}`;
+      return `https://s3.${AWS_REGION}.amazonaws.com/${AWS_S3_BUCKET}/${s3Key}`;
     });
     const uploadedImageUrls = await Promise.all(uploadPromises);
     return uploadedImageUrls;
@@ -79,6 +85,41 @@ const uploadToS3 = async (airtableListingId, airtableUploadedImages) => {
         console.error("Error deleting local file:", deleteError);
       }
     }
+  }
+};
+
+// Function to read secrets from an encrypted file and return the config
+const readSecretsFromFile = async (secretsFilePath, passphraseFilePath) => {
+  try {
+    const execPromise = promisify(exec);
+
+    if (!passphraseFilePath) {
+      throw new Error("Passphrase not available.");
+    }
+    const decryptCommand = `openssl enc -d -aes-256-cbc -in ${secretsFilePath} -pass file:${passphraseFilePath}`;
+    const { stdout, stderr } = await execPromise(decryptCommand);
+    if (stderr) {
+      console.error(`Error reading secrets: ${stderr}`);
+      return;
+    }
+    if (stdout) {
+      console.info(`Secrets read successfully: ${stdout}`);
+    }
+
+    // Split and convert to JSON
+    let keyValuePairs: string[][] = [];
+    const lines = stdout.split("\n");
+    for (const line of lines) {
+      if (line.trim() === "") continue; // Skip empty lines
+      const keyValuePair = line.split("=").map((pair) => pair.trim());
+      keyValuePairs.push(keyValuePair);
+    }
+
+    const secrets = Object.fromEntries(keyValuePairs);
+    return secrets;
+  } catch (error) {
+    console.error("Error reading secrets:", error);
+    throw error;
   }
 };
 
@@ -107,11 +148,20 @@ const uploadToAirtable = async (airtableListingId, urls) => {
   }
 };
 
+const encryptedSecretsFilePath = process.env.CTFG_SECRETS_FILE || "secrets.txt";
+const passphraseFilePath =
+  process.env.CTFG_PASSPHRASE_FILE || "defaultPassphraseFilePath.txt";
+
 // Call the uploadToS3 function
 export default async function handler(
   airtableListingId: string,
   airtableUploadedImages: any
 ) {
-  const urls = await uploadToS3(airtableListingId, airtableUploadedImages);
+  const urls = await uploadToS3(
+    airtableListingId,
+    airtableUploadedImages,
+    encryptedSecretsFilePath,
+    passphraseFilePath
+  );
   await uploadToAirtable(airtableListingId, urls);
 }
